407eea2d72534e0e056ad1b3714a79c03a3e26e2
제이제이천 <@DESKTOP-M8QPCFC>
2021-09-25 12:57:13 +0900

    application.yam active 별로 구분

--- src/main/java/com/jjplatform/admin/JJPlatformApplication.java
+++ src/main/java/com/jjplatform/admin/JJPlatformApplication.java
@@ -3,9 +3,18 @@ package com.jjplatform.admin;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
-@SpringBootApplication
+@Configuration
 @MapperScan(basePackages = "com.jjplatform.admin") 
+@SpringBootApplication(scanBasePackages={"com.jjplatform", "com.jjplatform.admin"})
+@PropertySource({ "classpath:/config/application.yml"
+//				, "classpath:/config/application.properties"
+				, "classpath:/config/application-${spring.profiles.active}.yml"
+//				, "classpath:/config/application-service-${spring.profiles.active}.yml"
+//				, "classpath:/config/application-core-${spring.profiles.active}.yml"
+				})
 public class JJPlatformApplication {
 
     public static void main(String[] args) {
